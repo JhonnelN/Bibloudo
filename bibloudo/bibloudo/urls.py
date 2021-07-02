e6f936f4b69f8a740aa6d5d5508c3610d542c48b
root <root@ip-172-26-9-189.ec2.internal>
2021-07-01 21:42:45 -0400

    Easy JWT implementacion

--- bibloudo/bibloudo/urls.py
+++ bibloudo/bibloudo/urls.py
@@ -17,11 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from bibloudo.views import index
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
     #path('biblioteca/', biblioteca)
 ]
 urlpatterns += [
